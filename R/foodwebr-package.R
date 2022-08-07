d37a32f77beaa5821a7dd44c45985f856b52b635
SigurdJanson <emailjs@web.de>
2022-08-07 12:53:52 +0200

    Added package-level documentation using ?usethis::use_package_doc

---
+++ R/foodwebr-package.R
@@ -0,0 +1,15 @@
+#' foodwebr
+#'
+#' foodwebr makes it easy to visualise the dependency graph of a set of functions
+#' (i.e. who calls who). This can be useful for exploring an unfamiliar codebase,
+#' or reminding yourself what you wrote ten minutes ago.
+#'
+#' @aliases foodwebr-package
+#' @keywords documentation debugging utilities
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
