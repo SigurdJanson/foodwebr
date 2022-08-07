e44f71a81cfa1d025a18aedf1bb2d43ec1f49f2f
SigurdJanson <emailjs@web.de>
2022-08-07 11:29:43 +0200

    Added tests for `get_calls()`. 1. Tracking down the error "object of type 'symbol' is not subsettable": The "bb" test does this. 2. Check if the new native pipe operator in R affects the function.

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -5,6 +5,11 @@ a <- function() {
   zz <- get_called_functions(a)
   TRUE
 }
+bb <- function() seq_len(3)
+cc <- function() { 3 |> seq_len() }
+dd <- function() 1:3 |> mean()
+
+
 
 test_that("`get_calls()` detects calls correctly", {
   cls <- get_calls(body(a))
@@ -13,6 +18,40 @@ test_that("`get_calls()` detects calls correctly", {
   expect_identical(cls[[3]], quote(exp))
   expect_identical(cls[[4]], quote(mean))
 })
+
+
+test_that("`get_calls(bb)`: with `bb` being a single call to other function without brackets", {
+  # act
+  cls <- get_calls(body(bb))
+  cls <- unlist(cls)
+
+  # assert
+  expect_identical(cls[[1]], quote(seq_len))
+})
+
+
+test_that("`get_calls(cc)` with `cc` including a |> (pipe)", {
+  # act
+  cls <- get_calls(body(cc))
+  cls <- unlist(cls)
+
+  # assert
+  expect_identical(cls[[1]], quote(`{`))
+  expect_identical(cls[[2]], quote(seq_len))
+})
+
+test_that("`get_calls()`:  no function brackets; pipe", {
+  # act
+  cls <- get_calls(body(dd))
+  cls <- unlist(cls)
+
+  # assert
+  expect_identical(cls[[2]], quote(`:`))
+  expect_identical(cls[[1]], quote(mean))
+})
+
+
+
 
 test_that("`get_called_functions()` captures the right functions", {
   expect_equal(
